d820b420be8287e4aae1619cd9cc5e2ed8ee9922
Raudel Garcia <raudelgar86@raudels-mini.home>
2019-04-14 16:11:27 -0400

    route implementation completed, back-end completed

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,4 +15,28 @@ router.get('/', (req, res) => {
 		.then(items => res.json(items));
 });
 
+/**
+ * @route   POST api/items
+ * @desc    Create a new Item
+ * @access  Publc
+ */
+router.post('/', (req, res) => {
+	const newItem = new Item({
+		name: req.body.name
+	});
+
+	newItem.save().then(item => res.json(item));
+});
+
+/**
+ * @route   DELETE api/items:id
+ * @desc    Delete an Item
+ * @access  Publc
+ */
+router.delete('/:id', (req, res) => {
+	Item.findById(req.params.id)
+		.then(item => item.remove().then(() => res.json({ success: true })))
+		.catch(err => res.status(404).json({ success: false }));
+});
+
 module.exports = router;

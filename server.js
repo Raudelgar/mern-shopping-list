385f76e7500d10d0512695b8d04a2aa0f1608d43
Raudel Garcia <raudelgar86@raudels-mini.home>
2019-04-13 21:27:14 -0400

    implementing route configuration, first get method

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const items = require('./routes/api/items');
 
 //BodyParser Middleware
 app.use(bodyParser.json());
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
 mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected...!"))
-  .catch(err => console.log(err));
+	.connect(db)
+	.then(() => console.log('MongoDB Connected...!'))
+	.catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
+
+//Use Routes
+app.use('/api/items', items);
 
 app.listen(port, () => console.log(`Server Started on port ${port}...!`));

21a9dd732db9d5c34c5b2bb970f43279afe65c62
Raudel Garcia <raudelgar86@raudels-mini.home>
2019-05-26 18:02:19 -0400

    adding shopinglist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AppNavbar from './component/appNavBar'
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
-  return (
-    <div className="App">
-      <AppNavbar/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<AppNavbar />
+			<ShoppingList />
+		</div>
+	);
 }
 
 export default App;

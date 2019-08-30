9d1f8d0c822e9fba6d9f08b3e97d76e65323919d
raudelgar <raudel24@gmail.com>
2019-08-30 14:13:15 -0400

    app prepared to deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,5 +26,14 @@ app.get('/health', (req, res) => {
 
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('./client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(port, () => console.log(`Server Started on port ${port}...!`));
